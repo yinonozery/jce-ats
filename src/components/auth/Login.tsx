4361a4dce46bb75c998b3c4249153818465d5d3c
yinonozery <yinonozery@gmail.com>
2023-06-11 11:21:52 +0300

    Fix login & forgot password bugs

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import firebase from "../../firebase/firebase";
+import React, { useState, useEffect } from 'react';
+import firebase from '../../firebase/firebase';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input, message, Divider, Modal } from 'antd';
-import { Link, useNavigate } from "react-router-dom";
-import appConfig from "../../stores/appStore";
-import { MISSING_FIELD, VALID_EMAIL } from "../../utils/messages";
+import { useNavigate } from 'react-router-dom';
+import appConfig from '../../stores/appStore';
+import { FORGOT_PASS_SUCCESS, WRONG_EMAIL, MISSING_FIELD, VALID_EMAIL, WRONG_PASSWORD } from '../../utils/messages';
 
 type loginForm = {
     email: string,
@@ -14,9 +14,8 @@ type loginForm = {
 
 const Login: React.FC = () => {
     const navigate = useNavigate();
-    const [passwordVisible, setPasswordVisible] = useState(false);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [resetEmail, setResetEmail] = useState('');
+    const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+    const [forgotPasswordModal, setForgotPasswordModal] = useState<boolean>(false);
 
     useEffect(() => {
         const userInfo = localStorage.getItem('userInfo');
@@ -24,27 +23,30 @@ const Login: React.FC = () => {
             navigate('/')
     }, [navigate])
 
-    const resetPassword = async (email: string) => {
-        appConfig.loadingHandler(true);
-        try {
-            await firebase.doResetPassword(email);
-            message.success(`${resetEmail} We've sent an email with instructions to reset your password. Please check your inbox and follow the instructions provided.`)
-        } catch (err: any) {
-            message.error(err?.code)
-        } finally {
-            appConfig.loadingHandler(false);
-        }
-    }
-
     const onFinish = async (userDetails: loginForm) => {
         appConfig.loadingHandler(true);
         try {
             await firebase.doSignInWithEmailAndPassword(userDetails.email, userDetails.password);
             navigate('/');
         } catch (err: any) {
-            message.error(err?.code)
+            if (err.code === 'auth/user-not-found')
+                message.error(WRONG_EMAIL)
+            else if (err.code === 'auth/wrong-password')
+                message.error(WRONG_PASSWORD)
         } finally {
             appConfig.loadingHandler(false);
+        }
+    }
+
+    const resetPassword = async (values: any) => {
+        const { emailReset } = values;
+        try {
+            await firebase.doResetPassword(emailReset);
+            message.success(FORGOT_PASS_SUCCESS)
+            setForgotPasswordModal(false);
+        } catch (err: any) {
+            if (err.code === 'auth/user-not-found')
+                message.error(WRONG_EMAIL)
         }
     }
 
@@ -52,62 +54,61 @@ const Login: React.FC = () => {
         <>
             <Divider>Login</Divider>
             <Form
-                name="normal_login"
-                className="login-form"
                 initialValues={{ remember: true }}
-                onFinish={(e) => { onFinish(e); }}
+                onFinish={onFinish}
             >
 
                 {/* Email */}
                 <Form.Item
-                    name="email"
+                    name='email'
                     rules={[{ type: 'email', required: true, message: VALID_EMAIL }]}
                 >
-                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+                    <Input prefix={<UserOutlined />} placeholder='Email' />
                 </Form.Item>
 
                 {/* Password */}
                 <Form.Item
-                    name="password"
+                    name='password'
                     rules={[{ required: true, message: MISSING_FIELD('password') }]}
                 >
                     <Input.Password
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        placeholder="input password"
+                        prefix={<LockOutlined />}
+                        placeholder='input password'
                         visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
                     />
                 </Form.Item>
 
-                {/* Forgot Password + Modal */}
-                <Modal title={<Divider>Reset password</Divider>} open={isModalOpen} footer={[]} onCancel={() => setIsModalOpen(false)}>
-                    <Form.Item
-                        name="emailReset"
-                        rules={[{ type: 'email', required: true, message: VALID_EMAIL }]}
-                        hasFeedback
-                    >
-                        <Input type="email" onChange={(email) => setResetEmail(email.currentTarget.value)} name="resetEmail" prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
-                    </Form.Item>
-                    <div style={{ textAlign: 'center' }}><Button type="default" onClick={() => { resetPassword(resetEmail) }}>Submit</Button></div>
-                </Modal>
+                {/* Forgot Password */}
                 <Form.Item>
-                    <Link className="login-form-forgot" to="#" onClick={() => setIsModalOpen(true)}>
+                    <Button type='link' onClick={() => setForgotPasswordModal(true)}>
                         Forgot password
-                    </Link>
+                    </Button>
                 </Form.Item>
 
                 {/* Submit button */}
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
-                    </Button>
+                <Button type='primary' htmlType='submit'>
+                    Log in
+                </Button>
 
-                    {/* Remember me */}
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                        <Checkbox style={{ 'marginLeft': 10 }}>Remember me</Checkbox>
-                    </Form.Item>
-
+                {/* Remember me */}
+                <Form.Item name='remember' valuePropName='checked' noStyle>
+                    <Checkbox style={{ 'marginLeft': '10px' }}>Remember me</Checkbox>
                 </Form.Item>
             </Form>
+
+            {/* Forgot Password Modal */}
+            <Modal title={<Divider>Reset password</Divider>} open={forgotPasswordModal} footer={[]} onCancel={() => setForgotPasswordModal(false)}>
+                <Form onFinish={resetPassword}>
+                    <Form.Item
+                        name='emailReset'
+                        rules={[{ type: 'email', required: true, message: VALID_EMAIL }]}
+                        hasFeedback
+                    >
+                        <Input type='email' prefix={<UserOutlined />} placeholder='Email' />
+                    </Form.Item>
+                    <Button type='primary' htmlType='submit' block>Submit</Button>
+                </Form>
+            </Modal>
         </>
     )
 }
