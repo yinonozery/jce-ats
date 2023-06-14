ebd3a125f4b1e73ac3bf6590650e8332d431ebfb
yinonozery <yinonozery@gmail.com>
2023-06-14 16:35:12 +0300

    Fix footer and layout responsive

--- src/components/FullLayout.tsx
+++ src/components/FullLayout.tsx
@@ -22,17 +22,17 @@ const FullLayout: React.FC<{ children: ReactNode }> = (props) => {
                 const restOfWord = word.slice(1).toLowerCase();
                 return firstLetter + restOfWord;
             });
-            document.title = process.env.REACT_APP_WINDOW_TITLE + " - " + words.join(' ');
+            document.title = process.env.REACT_APP_WINDOW_TITLE + ' - ' + words.join(' ');
             return words.join(' ');
         }
     };
 
     return (
         <>
-            <Layout hasSider>
+            <Layout hasSider style={{ marginBlockEnd: '40px' }}>
                 <Layout>
                     <SideNav state={marginResponsive} stateFunc={setMarginResponsive} />
-                    <Layout className="site-layout" style={{ marginLeft: (marginResponsive) }} >
+                    <Layout style={{ marginLeft: (marginResponsive) }} >
                         <HeaderNav marginResponsive={marginResponsive}>
                             <div style={{ position: 'fixed', marginLeft: '20px', left: marginResponsive, marginTop: '60px', color: 'black' }}>
                                 <Breadcrumb style={{ margin: '16px 0px', position: 'fixed' }}>
@@ -47,29 +47,42 @@ const FullLayout: React.FC<{ children: ReactNode }> = (props) => {
                                 </Breadcrumb>
                             </div>
                         </HeaderNav>
-                        <div style={{ padding: '14px 16px 0', marginBlockStart: '130px' }}>
-                            <Content
-                                className="responsive-content"
-                                style={{
-                                    background: 'white',
-                                    margin: '0 auto',
-                                    marginBlockStart: '50px',
-                                    position: 'relative',
-                                    zIndex: 0,
-                                    padding: '30px',
-                                    minWidth: 'fit-content',
-                                    boxShadow: "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px"
-                                }}
-                            >
-                                {!appConfig.errorModalVisible ? props.children : <ErrorDataModal />}
-                            </Content>
-                        </div>
-                        <Footer style={{ textAlign: 'center', marginTop: '5px', backgroundColor: 'transparent', bottom: '0' }}>
-                            JCE Careers Final Project &copy; 2023 Created by Yinon Ozery
-                        </Footer>
+                        <Layout>
+                            <div style={{ padding: '14px 16px 0', marginBlockStart: '130px' }}>
+                                <Content
+                                    className='responsive-content'
+                                    style={{
+                                        background: 'white',
+                                        marginInline: 'auto',
+                                        marginBlockStart: '50px',
+                                        position: 'relative',
+                                        zIndex: 0,
+                                        padding: '30px',
+                                        minWidth: 'fit-content',
+                                        maxWidth: 'max-content',
+                                        boxShadow: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px'
+                                    }}
+                                >
+                                    {!appConfig.errorModalVisible ? props.children : <ErrorDataModal />}
+                                </Content>
+                            </div>
+                        </Layout>
+
                     </Layout>
                 </Layout>
             </Layout >
+            <Footer style={{
+                position: 'fixed',
+                bottom: 0,
+                width: '100%',
+                backgroundColor: '#fefefe',
+                padding: '5px',
+                textAlign: 'center',
+                fontSize: '12px',
+                paddingLeft: marginResponsive
+            }}>
+                JCE Careers Final Project &copy; 2023 Created by Yinon Ozery
+            </Footer>
         </>
     )
 };
