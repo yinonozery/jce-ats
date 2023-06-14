38d068799b876af0405e4ff42f08962ae80cbeae
yinonozery <yinonozery@gmail.com>
2023-06-14 16:04:30 +0300

    Fix no refresh

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Button, Result, Divider } from 'antd';
 import { ResultStatusType } from 'antd/es/result';
+import { useNavigate } from 'react-router-dom';
 
 type errorProps = {
   statusCode: ResultStatusType | undefined,
   subTitle: string,
 }
 
-const Error: React.FC<errorProps> = (props) => (
-
-  <Result
-    status={props.statusCode}
-    title={<Divider style={{ fontSize: '25px' }}>{props.statusCode}</Divider>}
-    subTitle={props.subTitle}
-    extra={<Button type="primary" onClick={() => document.location.href = '/'}>Back Home</Button>}
-  />
-);
+const Error: React.FC<errorProps> = (props) => {
+  const navigate = useNavigate();
+  return (
+    <Result
+      status={props.statusCode}
+      title={<Divider style={{ fontSize: '25px' }}>{props.statusCode}</Divider>}
+      subTitle={props.subTitle}
+      extra={<Button type='primary' onClick={() => navigate('/')}>Back Home</Button>}
+    />)
+}
 
 export default Error;
