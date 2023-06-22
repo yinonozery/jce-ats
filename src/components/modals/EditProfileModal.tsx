cfb7065d17a9d377e850c18af26f46de030fa40a
yinonozery <yinonozery@gmail.com>
2023-06-22 11:54:03 +0300

    Fix user changed bug

--- src/components/modals/EditProfileModal.tsx
+++ src/components/modals/EditProfileModal.tsx
@@ -1,9 +1,10 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { Modal, Form, Input, message, Divider } from "antd";
 import userStore from "../../stores/userStore";
 import firebase from "../../firebase/firebase";
 import type { FormInstance } from 'antd/es/form';
-import { MISSING_FIELD, REAUTHENTICATION_MSG, VALID_EMAIL } from "../../utils/messages";
+import { REAUTHENTICATION_MSG, VALID_EMAIL } from "../../utils/messages";
+import { observer } from "mobx-react";
 
 interface modalProps {
     state: boolean,
@@ -12,19 +13,22 @@ interface modalProps {
 
 const EditProfileModal: React.FC<modalProps> = (props) => {
     const profileFormRef = React.useRef<FormInstance>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const handleUpdateProfile = () => {
+    const handleUpdateProfile = async () => {
+        setIsLoading(true);
         const { name, email } = profileFormRef?.current?.getFieldsValue(["name", "email"]);
         try {
-            firebase.doUpdateProfile(name, email);
-            message.success("Profile details have been updated successfully")
-            props.stateFunc(false);
+            await firebase.doUpdateProfile(name, email);
+            message.success("Profile details have been updated successfully");
         } catch (error) {
-            message.error("Sorry, we were unable to update your profile details. Please try again later")
+            message.error("Sorry, we were unable to update your profile details. Please try again later");
+        } finally {
+            props.stateFunc(false);
+            setIsLoading(false);
         }
     };
 
-    const user = userStore.userInfo;
     const reAuthenticationNeeded = !firebase.auth.currentUser!?.metadata?.lastSignInTime ? true : false;
 
     return (
@@ -32,24 +36,22 @@ const EditProfileModal: React.FC<modalProps> = (props) => {
             title={<Divider orientation="left">Edit Profile</Divider>}
             open={props.state}
             onOk={() => handleUpdateProfile()}
-            onCancel={() => props.stateFunc(false)}>
+            onCancel={() => props.stateFunc(false)}
+            confirmLoading={isLoading}
+        >
             <Form
                 ref={profileFormRef}
-                initialValues={{ name: user?.displayName, email: user?.email }}
+                initialValues={{ name: userStore.userInfo?.displayName, email: userStore.userInfo?.email }}
                 layout="vertical"
             >
-                <Form.Item label="Name" name="name" rules={[{
-                    type: 'string',
-                    required: true,
-                    message: MISSING_FIELD('Name')
-                }]} hasFeedback>
+                <Form.Item label="Name" name="name">
                     <Input />
                 </Form.Item>
                 <Form.Item label="Email" name="email" rules={[{
                     type: 'email',
                     required: true,
                     message: VALID_EMAIL,
-                }]} hasFeedback>
+                }]}>
                     <Input type="email" disabled={reAuthenticationNeeded} />
                 </Form.Item>
                 <Form.Item style={{ marginBlockStart: -30 }} validateStatus="warning" help={reAuthenticationNeeded ? REAUTHENTICATION_MSG : ""}></Form.Item>
@@ -58,4 +60,4 @@ const EditProfileModal: React.FC<modalProps> = (props) => {
     )
 };
 
-export default EditProfileModal;
+export default observer(EditProfileModal);
