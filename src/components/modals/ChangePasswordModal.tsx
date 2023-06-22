708eb218302ac46bf24f6754cbece3b8b626ff46
yinonozery <yinonozery@gmail.com>
2023-06-22 14:09:42 +0300

    Fix change password modal

--- src/components/modals/ChangePasswordModal.tsx
+++ src/components/modals/ChangePasswordModal.tsx
@@ -1,8 +1,8 @@
-import React, { Dispatch, SetStateAction } from "react";
-import { Modal, Form, Input, message, Divider } from "antd";
-import firebase from "../../firebase/firebase";
+import React, { Dispatch, SetStateAction } from 'react';
+import { Modal, Form, Input, message, Divider } from 'antd';
+import firebase from '../../firebase/firebase';
 import type { FormInstance } from 'antd/es/form';
-import { MISSING_FIELD, REAUTHENTICATION_MSG, PASSWORD_CONFIRMATION, FIELD_MIN_LENGTH } from "../../utils/messages";
+import { MISSING_FIELD, REAUTHENTICATION_MSG, PASSWORD_CONFIRMATION, FIELD_MIN_LENGTH } from '../../utils/messages';
 
 interface modalProps {
     state: boolean,
@@ -14,44 +14,44 @@ const ChangePasswordModal: React.FC<modalProps> = (props) => {
     const reAuthenticationNeeded = !firebase.auth.currentUser!?.metadata?.lastSignInTime ? true : false;
 
     const handleChangePassword = async () => {
-        const { newPassword } = profileFormRef?.current?.getFieldsValue(["password"]);
-
+        const { password } = await profileFormRef.current?.validateFields();
         try {
-            if (newPassword.length < 6)
-                throw new Error("Password should be at least 6 characters");
-            await firebase.doChangePassword(newPassword);
-            message.success("Password have been changed successfully")
+            if (password.length < 6)
+                throw new Error('Password should be at least 6 characters');
+            await firebase.doChangePassword(password);
+            message.success('Password have been changed successfully')
             props.stateFunc(false);
+            profileFormRef.current?.resetFields();
         } catch (error) {
             console.log(error);
-            message.error("Sorry, we were unable to change your password. Please try again later")
+            message.error('Sorry, we were unable to change your password. Please try again later')
         }
     };
 
     return (
         <Modal
-            title={<Divider orientation="left">Change Password</Divider>}
+            title={<Divider orientation='left'>Change Password</Divider>}
             open={props.state}
-            onOk={() => reAuthenticationNeeded ? handleChangePassword() : null}
+            onOk={() => !reAuthenticationNeeded ? handleChangePassword() : null}
             onCancel={() => props.stateFunc(false)}
             confirmLoading={false}
         >
             <Form
                 ref={profileFormRef}
-                layout="vertical"
+                layout='vertical'
             >
-                <Form.Item label="Password" name="password" rules={[
+                <Form.Item label='Password' name='password' rules={[
                     {
                         required: true,
                         message: MISSING_FIELD('Password')
                     },
                     {
                         min: 6,
-                        message: FIELD_MIN_LENGTH("Password", 6),
+                        message: FIELD_MIN_LENGTH('Password', 6),
                     }]}>
                     <Input.Password disabled={reAuthenticationNeeded} />
                 </Form.Item>
-                <Form.Item label="Confirm Password" name="confirm"
+                <Form.Item label='Confirm Password' name='confirm'
                     dependencies={['password']}
                     rules={[
                         {
@@ -67,10 +67,9 @@ const ChangePasswordModal: React.FC<modalProps> = (props) => {
                             },
                         }),
                     ]} hasFeedback>
-
-                    <Input type="password" disabled={reAuthenticationNeeded} />
+                    <Input type='password' disabled={reAuthenticationNeeded} />
                 </Form.Item>
-                <Form.Item style={{ marginBlockStart: -30 }} validateStatus="warning" help={reAuthenticationNeeded ? REAUTHENTICATION_MSG : ""}></Form.Item>
+                <Form.Item style={{ marginBlockStart: -30 }} validateStatus='warning' help={reAuthenticationNeeded ? REAUTHENTICATION_MSG : ''}></Form.Item>
             </Form>
         </Modal>
     )
