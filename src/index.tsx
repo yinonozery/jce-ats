3adde829c99e6cae97e9344746f1e8061159546a
yinonozery <yinonozery@gmail.com>
2023-07-02 21:42:30 +0300

    --allow-empty

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   // <React.StrictMode>
-  <Provider {...userStore}>
+  <>
     <Tracker />
-    <App />
-  </Provider>
+    <Provider {...userStore}>
+      <App />
+    </Provider>
+  </>
   // </React.StrictMode>
 );
 
